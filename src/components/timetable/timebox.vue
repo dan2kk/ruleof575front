1584bba23f7d3a7f335dbbdc23cbecf2695369aa
dan2kk <dan2kk@naver.com>
2023-01-02 13:15:07 +0900

    add icon and time box

---
+++ src/components/timetable/timebox.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="box"><p class="num-0900">09:00</p></div>
+  </template>
+  
+  <script>
+  export default {
+    name: "num-time"
+  };
+  </script>
+  
+  <style lang="scss" scoped>
+  .box{
+    background-color: rgba(210, 210, 210, 1);
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+  .num-0900 {
+    height: 39px;
+    width: 58px;
+    color: $black;
+    display: flex;
+    align-items: center;
+    text-align: center;
+    @include noto-sans-kr-12-regular;
+  }
+  </style>
---
+++ src/components/timetable/timebox1.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="box1"><p class="box-time">.</p></div>
+  </template>
+  
+  <script>
+  export default {
+    name: "box-time"
+  };
+  </script>
+  
+  <style lang="scss" scoped>
+  .box1{
+    background-color: rgba(210, 210, 210, 1);
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+  .box-time{
+    height: 40px;
+    width: 58px;
+    color: $black;
+    display: flex;
+    text-align: center;
+    @include noto-sans-kr-12-regular;
+  }
+  </style>
