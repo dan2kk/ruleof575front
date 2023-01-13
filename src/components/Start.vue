7c59944a7fac00a0707214d93545d88ae1a5dcf8
dewpe000 <dewpe000@naver.com>
2023-01-13 07:31:23 +0000

    Refactoring initList

--- src/components/Start.vue
+++ src/components/Start.vue
@@ -42,7 +42,7 @@ export default {
             result[key].sort((a, b) => { return a.start - b.start});
         }
         console.log(result)
-        let date = await axios.post('/test', {times: result, stu_id: this.$store.getters.getUserID})
+        let date = await axios.post('/recommend', {time_blocks: result, stu_id: this.$store.getters.getUserID})
         
       } 
       catch (error) {
@@ -51,65 +51,6 @@ export default {
     },
     async sendLoginInfo(){
       alert("receive login event to Start.vue")
-      try{
-        let timetabledata = (await axios.get('/list/init', {params: {stu_id: this.$store.getters.getUserID}})).data
-        for(let i=0; i< timetabledata.length;i++){
-          this.$store.commit("addTimetable", timetabledata[i])
-          //console.log(timetabledata[i])
-          if(timetabledata[i].state == 1){ //시간표에 추가해야 할 것들
-            //console.log(timetabledata[i])
-            for(let j=0 ; j< timetabledata[i].요일.length; j++){
-              let day = 0
-              switch(timetabledata[i].요일[j]){
-                case '월': 
-                  day=1 
-                  break
-                case '화': 
-                  day=2
-                  break
-                case '수': 
-                  day=3
-                  break
-                case '목': 
-                  day=4
-                  break
-                case '금': 
-                  day=5
-                  break
-                default:
-                  alert("null")
-                  continue
-              }
-              let startTime = Number(timetabledata[i].시작시간[j].slice(0, -6))
-              let endTime = Number(timetabledata[i].끝시간[j].slice(0, -6))
-              let startHalf = (timetabledata[i].시작시간[j].slice(-5, -3) != '00')
-              let endHalf = (timetabledata[i].끝시간[j].slice(-5, -3) != '00')
-              //console.log(startTime+" "+endTime+" "+startHalf+" "+endHalf)
-              for(let k=startTime - 8; k < endTime -8; k++){
-                this.$store.getters.getBlocknumTable[day][k] = timetabledata[i].수업번호
-              }
-              //if(startHalf) this.$store.getters.getBlocknumTable[day][startTime-8] = 3
-              //if(endHalf) this.$store.getters.getBlocknumTable[day][endTime-8] = 2
-              if(startHalf){
-                if(this.$store.getters.getBlocknumTable[day][startTime-8] != 1){ //이미 반칸
-                  this.$store.getters.getBlocknumTable[day][startTime-8] = timetabledata[i].수업번호
-                }
-                else this.$store.getters.getBlocknumTable[day][startTime-8] = 3
-              }
-              if(endHalf){
-                if(this.$store.getters.getBlocknumTable[day][endTime-8] != 1){ //이미 반칸
-                  this.$store.getters.getBlocknumTable[day][endTime-8] = timetabledata[i].수업번호
-                }
-                else this.$store.getters.getBlocknumTable[day][endTime-8] = 2
-              } 
-            }
-          }
-        }
-        this.$store.commit("initBnum")
-      }
-      catch(err){
-        alert(err)
-      }
     },
     async sendCourseInfo(number){ //미완
       alert("receive course event to Start.vue")
--- src/components/XMenu.vue
+++ src/components/XMenu.vue
@@ -53,6 +53,7 @@ export default {
         alert("event received")
         this.$emit("logined")
         this.logined = true
+        this.initList()
         this.getJolupevent()
         this.menu = 1
     },
@@ -78,6 +79,67 @@ export default {
         this.updateJolupevent();
       }
       this.menu = number
+    },
+    async initList() {
+      try{
+        let timetabledata = (await axios.get('/list/init', {params: {stu_id: this.$store.getters.getUserID}})).data
+        for(let i=0; i< timetabledata.length;i++){
+          this.$store.commit("addTimetable", timetabledata[i])
+          //console.log(timetabledata[i])
+          if(timetabledata[i].state == 1){ //시간표에 추가해야 할 것들
+            //console.log(timetabledata[i])
+            for(let j=0 ; j< timetabledata[i].요일.length; j++){
+              let day = 0
+              switch(timetabledata[i].요일[j]){
+                case '월': 
+                  day=1 
+                  break
+                case '화': 
+                  day=2
+                  break
+                case '수': 
+                  day=3
+                  break
+                case '목': 
+                  day=4
+                  break
+                case '금': 
+                  day=5
+                  break
+                default:
+                  alert("null")
+                  continue
+              }
+              let startTime = Number(timetabledata[i].시작시간[j].slice(0, -6))
+              let endTime = Number(timetabledata[i].끝시간[j].slice(0, -6))
+              let startHalf = (timetabledata[i].시작시간[j].slice(-5, -3) != '00')
+              let endHalf = (timetabledata[i].끝시간[j].slice(-5, -3) != '00')
+              //console.log(startTime+" "+endTime+" "+startHalf+" "+endHalf)
+              for(let k=startTime - 8; k < endTime -8; k++){
+                this.$store.getters.getBlocknumTable[day][k] = timetabledata[i].수업번호
+              }
+              //if(startHalf) this.$store.getters.getBlocknumTable[day][startTime-8] = 3
+              //if(endHalf) this.$store.getters.getBlocknumTable[day][endTime-8] = 2
+              if(startHalf){
+                if(this.$store.getters.getBlocknumTable[day][startTime-8] != 1){ //이미 반칸
+                  this.$store.getters.getBlocknumTable[day][startTime-8] = timetabledata[i].수업번호
+                }
+                else this.$store.getters.getBlocknumTable[day][startTime-8] = 3
+              }
+              if(endHalf){
+                if(this.$store.getters.getBlocknumTable[day][endTime-8] != 1){ //이미 반칸
+                  this.$store.getters.getBlocknumTable[day][endTime-8] = timetabledata[i].수업번호
+                }
+                else this.$store.getters.getBlocknumTable[day][endTime-8] = 2
+              } 
+            }
+          }
+        }
+        this.$store.commit("initBnum")
+      }
+      catch(err){
+        alert(err)
+      }
     },
     async getJolupevent(){
       try{
