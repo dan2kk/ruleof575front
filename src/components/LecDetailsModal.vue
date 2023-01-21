b1325b34ae74003a916061e8979ed981006339b5
cocoju1704 <121540845+cocoju1704@users.noreply.github.com>
2023-01-21 22:42:20 +0900

    Add Chart to Modal

--- src/components/LecDetailsModal.vue
+++ src/components/LecDetailsModal.vue
@@ -1,6 +1,9 @@
 <template>
     <div class="lec-details-modal">
-        <div class= "overlay" @click="$emit('modal-close')"/>
+        <div class= "overlay valign-text-middle notosanskr-semibold-white-16px" @click="$emit('modal-close')" >
+          <div class = empty-cart></div>
+          창을 닫으려면 바깥을 클릭
+        </div>
         <div class= "lec-details-modal-card">
             <div class= "row">
             <RMenuModifiableTitleBox color="yellow" size="280" :text=this.modalData.lec_info.과목명></RMenuModifiableTitleBox>
@@ -26,10 +29,14 @@
             <div class= "row">
               <RMenuTextBox color="yellow" size="72" :text=this.modalData.lec_info.학수번호></RMenuTextBox>
               <RMenuTextBox color="yellow" size="72" :text=this.modalData.lec_info.수업번호></RMenuTextBox>
-            <RMenuTextBox color="yellow" size="72" :text=this.modalData.lec_info.특수수업구분></RMenuTextBox>
-            <RMenuTextBox color="yellow" size="72" :text=this.modalData.lec_info.이수구분코드명></RMenuTextBox>
-            <RMenuTextBox color="yellow" size="72" :text=this.modalData.lec_info.학점></RMenuTextBox>
+              <RMenuTextBox color="yellow" size="72" :text=this.modalData.lec_info.특수수업구분></RMenuTextBox>
+              <RMenuTextBox color="yellow" size="72" :text=this.modalData.lec_info.이수구분코드명></RMenuTextBox>
+              <RMenuTextBox color="yellow" size="72" :text=this.modalData.lec_info.학점></RMenuTextBox>
             </div>
+            <div class = "row">
+            <RMenuTitleBox color="yellow" size="360">과목 성적분포</RMenuTitleBox>
+            </div>
+            <canvas ref="barChart"/>
         </div>
     </div>
   </template>
@@ -39,17 +46,88 @@
 import RMenuTitleBox from "./RMenu/Box/RMenuTitleBox";
 import RMenuModifiableTitleBox from "./RMenu/Box/RMenuModifiableTitleBox";
 import RMenuTextBox from "./RMenu/Box/RMenuTextBox";
+import {Chart, registerables} from 'chart.js'
+import { assertDeclareModule } from "@babel/types";
+Chart.register(...registerables)
+
 export default {
   name: "Modal",
   props: ["modalData"],
   components: {
-    RMenuTitleBox, RMenuTextBox, RMenuModifiableTitleBox
+    RMenuTitleBox, RMenuTextBox, RMenuModifiableTitleBox,
   },
-  data(){
-    return{
+  computed:{
+
+  },
+  data:() => ({
+    options: {
+      responsive: true
+    }
+  }),
+  mounted(){
+    this.createChart()
+  },
+  methods:{
+    setChart(){
+      let data =     
+      {
+      labels: [],
+      datasets: [{
+        label: '# of Votes',
+        data: [],
+        backgroundColor: [
+          'rgb(255, 99, 132, 0.6)',
+          'rgb(255, 159, 64, 0.6)',
+          'rgb(255, 205, 86, 0.6)',
+          'rgb(75, 192, 192, 0.6)',
+          'rgb(54, 162, 235, 0.6)',
+          `rgb(0, 0, 255, 0.6)`,
+          'rgb(153, 51, 255, 0.6)',
+          'rgb(255, 51, 255, 0.6)',
+          'rgb(160, 160, 160, 0.6)',
+        ],
+        borderColor: ['rgba(255, 255, 255)'],
+        borderWidth: 2
+      }]}
+      //PF과목
+      if (this.modalData.lec_info.Pass != 0)
+      {
+        data.labels = ['P', 'F']
+        data.datasets[0].data.push(this.modalData.lec_info.Pass)
+        data.datasets[0].data.push(this.modalData.lec_info.F)
+      }
+      //non-PF과목
+      else
+      {
+        data.labels = [ 'A+', 'A', 'B+', 'B', 'C+', 'C', 'D+', 'D', 'F' ]
+        data.datasets[0].data.push(this.modalData.lec_info['A+'])
+        data.datasets[0].data.push(this.modalData.lec_info['A+'])
+        data.datasets[0].data.push(this.modalData.lec_info['B+'])
+        data.datasets[0].data.push(this.modalData.lec_info['B0'])
+        data.datasets[0].data.push(this.modalData.lec_info['C+'])
+        data.datasets[0].data.push(this.modalData.lec_info['C0'])
+        data.datasets[0].data.push(this.modalData.lec_info['D+'])
+        data.datasets[0].data.push(this.modalData.lec_info['D0'])
+        data.datasets[0].data.push(this.modalData.lec_info['F'])
+
+      }
+      return data
+    },
+    createChart(){
+      new Chart(this.$refs.barChart, {
+        type:'pie',
+        data:this.setChart(),
+        options:this.options
+      })
+
     }
   }
+
 };
+
+const ctx = document.getElementById('myChart');
+
+
 </script>
 
 <style lang="sass">
@@ -62,9 +140,11 @@ export default {
     position: absolute
     left:0
     top:0
+    align-items: center
 .overlay
-    opacity: 0.3
+    opacity: 0.4
     background-color: black
+    
 .lec-details-modal-card
     background-color: $error-color
     position: absolute
@@ -75,5 +155,17 @@ export default {
     background-color: white
     height : 400px
     z-index: 10
-    border: 3px solid black
+    border: 3px solid white
+    overflow-y: overlay
+.lec-details-modal-card::-webkit-scrollbar 
+  width : 1px
+  
+.lec-details-modal-card::-webkit-scrollbar-thumb
+  background-color: rgba(0,0,0)
+  height: 95%
+.empty-cart
+  width: 400px
+  height: 450px
+  opacity: 0
+  background-color: black
 </style>
